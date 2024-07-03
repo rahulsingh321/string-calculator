058ef18668453fffca3b38c8e56f637ebd23b8b7
Rahul Singh <rahul97811@gmail.com>
2024-07-03 13:44:05 +0530

    Added second test case

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -9,4 +9,8 @@ class StringCalculatorTest < Minitest::Test
   def test_empty_string_returns_zero
     assert_equal 0, @calculator.add('')
   end
+
+  def test_single_number_returns_value
+    assert_equal 1, @calculator.add('1')
+  end
 end
