21c4e71786c0795d15dea7f58b3d81fb468cdd8a
Rahul Singh <rahul97811@gmail.com>
2024-07-03 14:07:10 +0530

    Added test case for addition of multiple numbers

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -13,4 +13,12 @@ class StringCalculatorTest < Minitest::Test
   def test_single_number_returns_value
     assert_equal 1, @calculator.add('1')
   end
+
+  def test_two_numbers_comma_delimited
+    assert_equal 6, @calculator.add("1,5")
+  end
+
+  def test_multiple_numbers_comma_delimited
+    assert_equal 10, @calculator.add("1,2,3,4")
+  end
 end
