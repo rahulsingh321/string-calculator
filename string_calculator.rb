88c4131cc146167ccf58b1102cf9a723133b97ab
Rahul Singh <rahul97811@gmail.com>
2024-11-12 22:52:07 +0530

    Fix for test case #6

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,10 @@
 class StringCalculator
+  def initialize
+    @call_count = 0
+  end
+
   def add(numbers)
+    @call_count += 1
     return 0 if numbers.empty?
 
     delimiter = ','
@@ -16,6 +21,10 @@ class StringCalculator
     end
 
     nums.sum
+  end
+
+  def get_called_count
+    @call_count
   end
 
   private
