754e6094941639d93aea62383249d1de8a4a2233
Rahul Singh <rahul97811@gmail.com>
2024-11-12 22:25:21 +0530

    Fix for test case #4

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,9 +3,21 @@ class StringCalculator
     return 0 if numbers.empty?
 
     delimiter = ','
+    if numbers.start_with?('//')
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    end
+
     numbers   = numbers.gsub("\n", delimiter)
     nums      = numbers.split(delimiter).map(&:to_i)
 
     nums.sum
+  end
+
+  private
+
+  def parse_custom_delimiter(numbers)
+    parts     = numbers.split("\n", 2)
+    delimiter = parts[0][2..-1]
+    [delimiter, parts[1]]
   end
 end
